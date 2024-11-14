c0a0e2f97f011d74b43ea08bf14f4716238eec4b
Sean DuBois <seaduboi@openai.com>
2024-11-14 15:54:24 -0500

    Initial Commit

---
+++ src/main.cpp
@@ -0,0 +1,32 @@
+#include "main.h"
+
+#include <esp_event.h>
+#include <esp_log.h>
+#include <peer.h>
+
+#ifndef LINUX_BUILD
+#include "nvs_flash.h"
+
+extern "C" void app_main(void) {
+  esp_err_t ret = nvs_flash_init();
+  if (ret == ESP_ERR_NVS_NO_FREE_PAGES ||
+      ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+    ESP_ERROR_CHECK(nvs_flash_erase());
+    ret = nvs_flash_init();
+  }
+  ESP_ERROR_CHECK(ret);
+
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+  peer_init();
+  oai_init_audio_capture();
+  oai_init_audio_decoder();
+  oai_wifi();
+  oai_webrtc();
+}
+#else
+int main(void) {
+  ESP_ERROR_CHECK(esp_event_loop_create_default());
+  peer_init();
+  oai_webrtc();
+}
+#endif
